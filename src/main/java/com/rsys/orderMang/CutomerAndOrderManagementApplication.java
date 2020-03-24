c263bba20b88ac48e3a833f9b70c51f667e1b02f
priyankakashyap1102 <kashyap.priyanka1193@gmail.com>
2020-03-24 15:24:22 +0530

    POC

---
+++ src/main/java/com/rsys/orderMang/CutomerAndOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rsys.orderMang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutomerAndOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutomerAndOrderManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsys/orderMang/CutomerAndOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsys.orderMang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutomerAndOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
